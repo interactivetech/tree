95ff3e488ba37dd97bc28f2e40b887b40cef24e2
interactivetech <interactivetech1@gmail.com>
2016-10-05 17:07:18 -0400

    initial commit from many iterations on code in local environment

--- pytree.py
+++ pytree.py
@@ -9,44 +9,44 @@ def recurse(path, lvl, spacing):
     items = os.listdir(path)
 
     for item in items[:]:
-      if item[0] == '.':
-        items.remove(item)
+        if item[0] == '.':
+            items.remove(item)
 
-      #print "LVL",lvl
+#print "LVL",lvl
 
     items=sorted(items)
     for j in range(len(items)):
-      filePath = os.path.join(path , items[j])
+        filePath = os.path.join(path,items[j])
 
-      if os.path.isdir(filePath):
+        if os.path.isdir(filePath):
 
-        if j < (len(items) - 1):
-          print spacing,"├──",
-        else :
-          print spacing, "└──",
-          print items[j]
+            if j < (len(items) - 1):
+                print spacing,"├──",
+            else :
+                print spacing, "└──",
+                print items[j]
 
-         #recursive add | and space space
-         #if last file, only send a space space
-          if j == len(items) - 1:
-            recurse(filePath, lvl + 1, spacing+"    ")
-          else:
-            recurse(filePath, lvl + 1, spacing+" │  ")
-          # if j<len(items)-1:
-          # 	recurse(filePath,lvl+1,space+" │  ")
-          # else:
-          # 	recurse(filePath,lvl+1,space+"    ")
-
-
+            #recursive add | and space space
+            #if last file, only send a space space
+            if j == len(items) - 1:
+                recurse(filePath, lvl + 1, spacing + "    ")
+            else:
+                recurse(filePath, lvl + 1, spacing + " │  ")
+            # if j<len(items)-1:
+            # 	recurse(filePath,lvl+1,space+" │  ")
+            # else:
+            # 	recurse(filePath,lvl+1,space+"    ")
 
 
-         elif os.path.isfile(filePath):
 
-           if j < (len(items) - 1):
-             print spacing,"├──",
-           else :
-             print spacing,"└──",
-             print items[j]
+
+        elif os.path.isfile(filePath):
+
+            if j < (len(items) - 1):
+                print spacing, "├──",
+            else:
+                print spacing, "└──",
+                print items[j]
 
 
 
