40304511b09907d7eec1ab8e0d5e652afc0b374b
interactivetech <interactivetech1@gmail.com>
2016-10-05 16:44:23 -0400

    initial commit from many iterations on code in local environment

--- pytree.py
+++ pytree.py
@@ -1,11 +1,60 @@
-#!/usr/bin/env python3
-import subprocess
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 
 
-# YOUR CODE GOES here
+def recurse(path,lvl,spacing):
+
+	items = os.listdir(path)
+
+	for item in items[:]:
+		if item[0]=='.':
+			items.remove(item)
+
+	#print "LVL",lvl
+
+	items=sorted(items)
+	for j in range(len(items)):
+		filePath = os.path.join(path,items[j])
+
+		if os.path.isdir(filePath):
+
+			if j < (len(items)-1):
+		  		print spacing,"├──",
+	  		else :
+	  			print spacing, "└──",
+	  		print items[j]
+
+	  		#recursive add | and space space
+	  		#if last file, only send a space space
+	  		if j ==len(items)-1:
+				recurse(filePath,lvl+1,spacing+"    ")
+			else:
+				recurse(filePath,lvl+1,spacing+" │  ")
+	  		# if j<len(items)-1:
+	  		# 	recurse(filePath,lvl+1,space+" │  ")
+	  		# else:
+	  		# 	recurse(filePath,lvl+1,space+"    ")
 
 
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+
+
+		elif os.path.isfile(filePath):
+
+			if j < (len(items)-1):
+		  		print spacing,"├──",
+	  		else :
+	  			print spacing,"└──",
+	  		print items[j]
+
+
+
+def call(path):
+	
+	items = os.listdir(path)
+	#print items
+	recurse(path,0,"")
+
+
+call(sys.argv[1])
