9b617d110febc858a5399a3a3f3f0471334d3080
interactivetech <interactivetech1@gmail.com>
2016-10-05 16:53:49 -0400

    initial commit from many iterations on code in local environment

--- pytree.py
+++ pytree.py
@@ -4,49 +4,49 @@ import os
 import sys
 
 
-def recurse(path,lvl,spacing):
+def recurse(path , lvl , spacing):
 
   items = os.listdir(path)
 
   for item in items[:]:
-    if item[0]=='.':
+    if item[0] == '.':
       items.remove(item)
 
 	#print "LVL",lvl
 
   items=sorted(items)
   for j in range(len(items)):
-    filePath = os.path.join(path,items[j])
+    filePath = os.path.join(path , items[j])
 
     if os.path.isdir(filePath):
 
       if j < (len(items)-1):
-        print spacing,"├──",""
+        print spacing,"├──",
       else :
         print spacing, "└──",
         print items[j]
 
          #recursive add | and space space
          #if last file, only send a space space
-      if j ==len(items)-1:
-        recurse(filePath, lvl+1 , spacing+"    ")
-      else:
-        recurse(filePath, lvl+1, spacing+" │  ")
-# if j<len(items)-1:
-# 	recurse(filePath,lvl+1,space+" │  ")
-# else:
-# 	recurse(filePath,lvl+1,space+"    ")
+        if j == len(items)-1:
+          recurse(filePath , lvl+1 , spacing+"    ")
+        else:
+          recurse(filePath , lvl+1, spacing+" │  ")
+      # if j<len(items)-1:
+      # 	recurse(filePath,lvl+1,space+" │  ")
+      # else:
+      # 	recurse(filePath,lvl+1,space+"    ")
 
 
 
 
-       elif os.path.isfile(filePath):
+         elif os.path.isfile(filePath):
 
-         if j < (len(items)-1):
-           print spacing,"├──",
-         else :
-           print spacing,"└──",
-           print items[j]
+           if j < (len(items)-1):
+             print spacing,"├──",
+           else :
+             print spacing,"└──",
+             print items[j]
 
 
 
