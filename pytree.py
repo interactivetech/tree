37682ae721072f7d9252d593795779fc6c873be3
interactivetech <interactivetech1@gmail.com>
2016-12-12 20:52:20 -0500

    New

--- pytree.py
+++ pytree.py
@@ -4,57 +4,54 @@ import os
 import sys
 
 
-def recurse(path,lvl,spacing):
+def recurse(path, lvl, spacing):
 
-	items = os.listdir(path)
+    items = os.listdir(path)
 
-	for item in items[:]:
-		if item[0]=='.':
-			items.remove(item)
+    for item in items[:]:
+        if item[0] == '.':
+            items.remove(item)
 
-	#print "LVL",lvl
+    # print "LVL",lvl
+    # test
 
-	items=sorted(items)
-	for j in range(len(items)):
-		filePath = os.path.join(path,items[j])
+    items = sorted(items)
+    for j in range(len(items)):
+        filePath = os.path.join(path, items[j])
 
-		if os.path.isdir(filePath):
+        if os.path.isdir(filePath):
 
-			if j < (len(items)-1):
-		  		print spacing,"├──",
-	  		else :
-	  			print spacing, "└──",
-	  		print items[j]
+            if j < (len(items) - 1):
+                print spacing, "├──",
+            else:
+                print spacing, "└──",
+            print items[j]
 
-	  		#recursive add | and space space
-	  		#if last file, only send a space space
-	  		if j ==len(items)-1:
-				recurse(filePath,lvl+1,spacing+"    ")
-			else:
-				recurse(filePath,lvl+1,spacing+" │  ")
-	  		# if j<len(items)-1:
-	  		# 	recurse(filePath,lvl+1,space+" │  ")
-	  		# else:
-	  		# 	recurse(filePath,lvl+1,space+"    ")
+            # recursive add | and space space
+            # if last file, only send a space space
+            if j == len(items) - 1:
+                recurse(filePath, lvl + 1, spacing + "    ")
+            else:
+                recurse(filePath, lvl + 1, spacing + " │  ")
+            # if j<len(items)-1:
+            # 	recurse(filePath,lvl+1,space+" │  ")
+            # else:
+            # 	recurse(filePath,lvl+1,space+"    ")
 
+        elif os.path.isfile(filePath):
 
-
-
-		elif os.path.isfile(filePath):
-
-			if j < (len(items)-1):
-		  		print spacing,"├──",
-	  		else :
-	  			print spacing,"└──",
-	  		print items[j]
-
+            if j < (len(items) - 1):
+                print spacing, "├──",
+            else:
+                print spacing, "└──",
+            print items[j]
 
 
 def call(path):
-	
-	items = os.listdir(path)
-	#print items
-	recurse(path,0,"")
+
+    items = os.listdir(path)
+    # print items
+    recurse(path, 0, "")
 
 
 call(sys.argv[1])
